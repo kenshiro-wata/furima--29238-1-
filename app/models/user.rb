0e9e99c8781e5a4647ae9f774c046e578ca12702
kenshiro-wata <kenshiroooo1234@gmail.com>
2020-09-02 16:09:52 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }, length: { minimum: 6}
+  validates :confirmation_password, presence: true
+  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :last_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX }
+  validates :first_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX }
+  validates :birth, presence: true
+
 end
